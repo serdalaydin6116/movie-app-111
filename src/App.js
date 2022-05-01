39e6a2e6700ddcee95f779dbf4fbe51d788f4b8b
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-01 19:51:57 +0300

    AppRuoter is formed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+
+
+function App(){
+  const notify = () => toast("Wow so easy!");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+        <h2>Self Study</h2>
+        <br /><br />
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer />
+      </div>
+    );
+  }
 
 export default App;
---
+++ src/router/AppRouter.js
@@ -0,0 +1,7 @@
+import {BrowserRouter as Router,
+Routes, Route, } from "react-router-dom";
+
+import Main from "../pages/Main";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import MovieDetail from "../pages/MovieDetail";

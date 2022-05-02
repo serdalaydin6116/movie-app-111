90436dad1de572d9440e2750f3d3c50972da67cc
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-03 01:10:42 +0300

    .env was build

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -25,13 +25,23 @@ import {
 //* https://firebase.google.com/docs/auth/web/start
 //* https://console.firebase.google.com/ => project settings
 //! firebase console settings bölümünden firebaseconfig ayarlarını al
-const firebaseConfig ={
-  apiKey: "AIzaSyD-T7J7dXJWvfq5Jh4ORzAhKomssxwNkAs",
-  authDomain: "movie-app-111.firebaseapp.com",
-  projectId: "movie-app-111",
-  storageBucket: "movie-app-111.appspot.com",
-  messagingSenderId: "116075209064",
-  appId: "1:116075209064:web:9f755b81830cf99c16b6dd"
+
+// const firebaseConfig ={
+//   apiKey: "AIzaSyD-T7J7dXJWvfq5Jh4ORzAhKomssxwNkAs",
+//   authDomain: "movie-app-111.firebaseapp.com",
+//   projectId: "movie-app-111",
+//   storageBucket: "movie-app-111.appspot.com",
+//   messagingSenderId: "116075209064",
+//   appId: "1:116075209064:web:9f755b81830cf99c16b6dd"
+// };
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 

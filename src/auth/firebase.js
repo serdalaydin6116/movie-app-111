2ad98fde0dc726b25203b035d8be41c512c937a4
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-02 04:28:12 +0300

    firebase auth steps were fulfilled.

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,5 +1,20 @@
 import {initializeApp} from "firebase/app";
-import {getAuth} from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    sendPasswordResetEmail,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+  } from "firebase/auth";
+//   import {
+//     toastErrorNotify,
+//     toastSuccessNotify,
+//     toastWarnNotify,
+//   } from "../helpers/ToastNotify";
 
 
 
@@ -25,3 +40,94 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+export const createUser = async (email, password, displayName, navigate) => {
+    try {
+      //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+      let userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      //? kullanıcı profilini güncellemek için kullanılan firebase metodu
+      await updateProfile(auth.currentUser, {
+        displayName: displayName,
+      });
+      navigate("/");
+    //   toastSuccessNotify("Registered successfully!");
+      console.log(userCredential);
+    } catch (err) {
+    //   toastErrorNotify(err.message);
+      alert(err.message);
+    }
+  };
+  
+  //* https://console.firebase.google.com/
+  //* => Authentication => sign-in-method => enable Email/password
+  //! Email/password ile girişi enable yap
+  export const signIn = async (email, password, navigate) => {
+    try {
+      //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+      let userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      navigate("/");
+    //   toastSuccessNotify("Logged in successfully!");
+      console.log(userCredential);
+    } catch (err) {
+    //   toastErrorNotify(err.message);
+      alert(err.message);
+    }
+  };
+  
+//   export const logOut = () => {
+//     signOut(auth);
+//     // toastSuccessNotify("Logged out successfully!");
+//   };
+  
+//   export const userObserver = (setCurrentUser) => {
+//     //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
+//     onAuthStateChanged(auth, (currentUser) => {
+//       if (currentUser) {
+//         setCurrentUser(currentUser);
+//       } else {
+//         // User is signed out
+//         setCurrentUser(false);
+//       }
+//     });
+//   };
+  
+//   //* https://console.firebase.google.com/
+//   //* => Authentication => sign-in-method => enable Google
+//   //! Google ile girişi enable yap
+//   export const signUpProvider = (navigate) => {
+//     //? Google ile giriş yapılması için kullanılan firebase metodu
+//     const provider = new GoogleAuthProvider();
+//     //? Açılır pencere ile giriş yapılması için kullanılan firebase metodu
+//     signInWithPopup(auth, provider)
+//       .then((result) => {
+//         console.log(result);
+//         navigate("/");
+//       })
+//       .catch((error) => {
+//         // Handle Errors here.
+//         console.log(error);
+//       });
+//   };
+  
+//   export const forgotPassword = (email) => {
+//     //? Email yoluyla şifre sıfırlama için kullanılan firebase metodu
+//     sendPasswordResetEmail(auth, email)
+//       .then(() => {
+//         // Password reset email sent!
+//         toastWarnNotify("Please check your mail box!");
+//         // alert("Please check your mail box!");
+//       })
+//       .catch((err) => {
+//         toastErrorNotify(err.message);
+//         // alert(err.message);
+//         // ..
+//       });
+//   };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { createUser } from '../auth/firebase';
 
 const Register = () => {
   const [firstName, setFirstName] = useState();
   const [lastName, setLastName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const navigate = useNavigate()
 
   const handleSubmit =(e)=>{
     e.preventDefault();
+    createUser(email, password);
+    navigate("/")
     console.log(firstName, lastName);
 
   };

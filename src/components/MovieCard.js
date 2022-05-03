0b39fc9c832894f6308de9fe65ff7a810e6b4bdd
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-05-03 05:10:20 +0300

    movie card params were set

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 
 
@@ -9,6 +10,11 @@ const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
 const MovieCard = ({title, poster_path, overview, vote_average, id}) => {
+    const { currentUser } = useContext(AuthContext);
+    let navigate = useNavigate();
+
+
+
   return (
     <div className="movie">
        <img src={poster_path ? IMG_API + poster_path : defaultImage} alt="" />
@@ -16,10 +22,16 @@ const MovieCard = ({title, poster_path, overview, vote_average, id}) => {
         <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
             <h5>{title}</h5>
             
-            <span  className={`tag ${vote_average >= 8 ? "green" : vote_average >= 6 ? "orange" : "red"
+            {currentUser&& <span  className={`tag ${vote_average >= 8 ? "green" : vote_average >= 6 ? "orange" : "red"
             }`}>
             {vote_average}
-          </span>
+          </span>}
+        </div>
+        <div className="movie-over">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+
+
         </div>
     </div>
     
